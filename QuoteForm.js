8779cf1f68adc5682ffb4557dfe0d7b2d65af465
Nabeel Mumtaz <153012098+nabeelmumtaz92@users.noreply.github.com>
2024-05-21 17:13:54 -0500

    Add files via upload

---
+++ QuoteForm.js
@@ -0,0 +1,81 @@
+import React, { useReducer } from 'react';
+import { useCreateQuoteMutation } from '../state/quotesApi';
+
+const CHANGE_INPUT = 'CHANGE_INPUT';
+const RESET_FORM = 'RESET_FORM';
+
+const initialState = {
+  authorName: '',
+  quoteText: '',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case CHANGE_INPUT: {
+      const { name, value } = action.payload;
+      return { ...state, [name]: value };
+    }
+    case RESET_FORM:
+      return { authorName: '', quoteText: '' };
+    default:
+      return state;
+  }
+};
+
+export default function TodoForm() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [createQuote] = useCreateQuoteMutation();
+
+  const onChange = ({ target: { name, value } }) => {
+    dispatch({ type: CHANGE_INPUT, payload: { name, value } });
+  };
+
+  const resetForm = () => {
+    dispatch({ type: RESET_FORM });
+  };
+
+  const onNewQuote = evt => {
+    evt.preventDefault();
+    const { authorName, quoteText } = state;
+    createQuote({ authorName, quoteText })
+      .unwrap()
+      .then(data => {
+        console.log(data);
+        resetForm();
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  };
+
+  return (
+    <form id="quoteForm" onSubmit={onNewQuote}>
+      <h3>New Quote Form</h3>
+      <label>
+        <span>Author:</span>
+        <input
+          type='text'
+          name='authorName'
+          placeholder='type author name'
+          onChange={onChange}
+          value={state.authorName}
+        />
+      </label>
+      <label>
+        <span>Quote text:</span>
+        <textarea
+          name='quoteText'
+          placeholder='type quote'
+          onChange={onChange}
+          value={state.quoteText}
+        />
+      </label>
+      <button
+        type='submit'
+        disabled={!state.authorName.trim() || !state.quoteText.trim()}
+      >
+        DO IT!
+      </button>
+    </form>
+  );
+}
---
+++ Quotes.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  setHighlightedQuote,
+  toggleVisibility,
+} from '../state/quotesSlice'
+import { useGetQuotesQuery, useToggleFakeMutation, useDeleteQuoteMutation } from '../state/quotesApi'
+
+export default function Quotes() {
+  const { data: quotes, isLoading: gettingQuotes, isFetching: refreshingQuotes } = useGetQuotesQuery()
+  const [toggleFake] = useToggleFakeMutation()
+  const [deleteQuote] = useDeleteQuoteMutation()
+  const displayAllQuotes = useSelector(st => st.quotesState.displayAllQuotes)
+  const highlightedQuote = useSelector(st => st.quotesState.highlightedQuote)
+  const dispatch = useDispatch()
+
+  return (
+    <div id="quotes">
+      <h3> 
+        Quotes
+        {(gettingQuotes || refreshingQuotes) && ' loading...'}
+      </h3>
+      <div>
+        {
+          quotes?.filter(qt => {
+            return displayAllQuotes || !qt.apocryphal
+          }).map(qt => (
+            <div 
+              key={qt.id}
+              className={`quote${qt.apocryphal ? " fake" : ''}${highlightedQuote === qt.id ? " highlight" : ''}`}
+            >
+              <div>{qt.quoteText}</div>
+              <div>{qt.authorName}</div>
+              <div className="quote-buttons">
+                <button onClick={() => deleteQuote(qt.id)}>DELETE</button>
+                <button onClick={() => dispatch(setHighlightedQuote(qt.id))}>HIGHLIGHT</button>
+                <button onClick={() => toggleFake({ id: qt.id, quote: { apocryphal: !qt.apocryphal } })}>FAKE</button>
+              </div>
+            </div>
+          ))
+        }
+        {
+          !quotes?.length && "No quotes here! Go write some."
+        }
+      </div>
+      {!!quotes?.length && <button onClick={() => dispatch(toggleVisibility())}>
+        {displayAllQuotes ? 'HIDE' : 'SHOW'} FAKE QUOTES
+      </button>}
+    </div>
+  )
+}
